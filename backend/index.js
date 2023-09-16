b9b9ae1161962d254dd729a7d4e56264749b0e2a
Mohit Mehta <mehtam2701@gmail.com>
2023-09-16 20:05:20 +0530

    testing if the backend is configured successfully on vercel

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,9 @@ app.use((req, res, next) => {
 	console.log(req.path, req.method);
 	next();
 });
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the backend!" });
+});
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}`);
